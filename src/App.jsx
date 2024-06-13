7956a27f494ef01ca8179a25e301fd7a77df122f
Anowar1987 <anowarhossen.iim@gmail.com>
2024-06-13 22:19:56 +0600

    Navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Book Vibe</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../NavBar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2 className="text-3xl">This is Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
